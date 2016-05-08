a9f01c9e6e7dd6382dd0650ec631013a0dc8825c
Dai Tran <dai.tr@geekup.vn>
2016-05-08 16:26:51 +0700

    Send data direct to Graphite

--- index.js
+++ index.js
@@ -4,15 +4,28 @@ var ipm2 = require('pm2-interface')();
 var _ = require('lodash');
 var moment = require('moment');
 var async = require('async');
-var StatsD = require('node-statsd')
+
+/**
+ * Connect graphite
+ */
+var graphite = require('graphite');
+
+var graphiteUrl = 'plaintext://' + process.env.GRAPHITE_HOST + ':'
+                                 + process.env.GRAPHITE_PORT;
+var client = graphite.createClient(graphiteUrl);
+
+/**
+ * Connect StatsD
+ */
+// var StatsD = require('node-statsd')
+// var statsdClient = new StatsD({
+//     host: process.env.STATSD_HOST || 'localhost',
+//     port: process.env.STATSD_PORT || 8125,
+//     prefix: process.env.STAT_PREFIX || '',
+//     suffix: process.env.STAT_SUFFIX || ''
+// });
 
 var services = (process.env.SERVICES || '').split(',');
-var statsdClient = new StatsD({
-    host: process.env.STATSD_HOST || 'localhost',
-    port: process.env.STATSD_PORT || 8125,
-    prefix: process.env.STAT_PREFIX || '',
-    suffix: process.env.STAT_SUFFIX || ''
-});
 
 var STAT_MEM_USAGE = 'memory';
 var STAT_CPU_USAGE = 'cpu'
@@ -44,8 +57,14 @@ function processPm2Data(data) {
             if (services.indexOf(serviceInfo.name) > -1) {
                 var memoryInByte = serviceInfo.monit.memory;
                 var memoryInMB = Math.round(100 * memoryInByte / (1024 * 1024)) / 100;
-                statsdClient.histogram(STAT_MEM_USAGE + '.' + serviceInfo.name, memoryInMB);
-                statsdClient.histogram(STAT_CPU_USAGE + '.' + serviceInfo.name, serviceInfo.monit.cpu);
+
+                var memoryStats = STAT_MEM_USAGE + '.' + serviceInfo.name;
+                var memoryMetrics = { memoryStats: memoryInMB};
+                client.write(memoryMetrics);
+
+                var cpuStats = STAT_CPU_USAGE + '.' + serviceInfo.name;
+                var cpuMetrics = { cpuStats: serviceInfo.monit.cpu };
+                client.write(cpuMetrics);
             }
         });
     }
